bc587ce5b25a6965e9918bb65e8a22882683b7db
Moaz Nabil <64426115+moaaz-nabil@users.noreply.github.com>
2020-06-01 20:41:57 +0200

    version

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "library",
-
+    'version': '12.0.1.0.0',
     'summary': """
         Library Managment App""",
 
@@ -16,7 +16,6 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
